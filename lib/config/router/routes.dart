09b8b149de7430db2e4d54c355e4956fb38f5d14
ParimSsme <parima.ssme@gmail.com>
2024-03-03 16:59:38 +0430

    add app routes

---
+++ lib/config/router/routes.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:furniture_shop_app/features/feature_auth/presentation/screens/login_screen.dart';
+import 'package:furniture_shop_app/features/feature_auth/presentation/screens/signup_screen.dart';
+import 'package:furniture_shop_app/features/feature_boarding/presentation/screens/boarding_screen.dart';
+import 'package:furniture_shop_app/features/feature_home/presentation/screens/home_screen.dart';
+import 'fade_page_route.dart';
+
+enum Routes {
+  boarding,
+  login,
+  signUp,
+  home,
+  productDetail,
+  favorite,
+  cart,
+  checkOut,
+  settings,
+  notification,
+  review,
+  profile,
+  order,
+  shippingAddress,
+  paymentMethod,
+  myReviews,
+  addShippingAddress,
+  addPayment,
+  setting,
+}
+
+class _Paths {
+  static const String boarding = '/boarding';
+  static const String login = '/login';
+  static const String signUp = '/signUp';
+  static const String home = '/home';
+  static const String productDetail = '/productDetail';
+  static const String favorite = '/favorite';
+  static const String cart = '/cart';
+  static const String checkOut = '/checkOut';
+  static const String settings = '/settings';
+  static const String notification = '/notification';
+  static const String review = '/review';
+  static const String profile = '/profile';
+  static const String order = '/order';
+  static const String shippingAddress = '/shippingAddress';
+  static const String paymentMethod = '/paymentMethod';
+  static const String myReviews = '/myReviews';
+  static const String addShippingAddress = '/addShippingAddress';
+  static const String addPayment = '/addPayment';
+
+  static const Map<Routes, String> _pathMap = {
+    Routes.boarding: _Paths.boarding,
+    Routes.login: _Paths.login,
+    Routes.signUp: _Paths.signUp,
+    Routes.home: _Paths.home,
+    Routes.productDetail: _Paths.productDetail,
+    Routes.favorite: _Paths.favorite,
+    Routes.cart: _Paths.cart,
+    Routes.checkOut: _Paths.checkOut,
+    Routes.settings: _Paths.settings,
+    Routes.notification: _Paths.notification,
+    Routes.review: _Paths.review,
+    Routes.profile: _Paths.profile,
+    Routes.order: _Paths.order,
+    Routes.shippingAddress: _Paths.shippingAddress,
+    Routes.paymentMethod: _Paths.paymentMethod,
+    Routes.myReviews: _Paths.myReviews,
+    Routes.addShippingAddress: _Paths.addShippingAddress,
+    Routes.addPayment: _Paths.addPayment,
+  };
+
+  static String of(Routes route) => _pathMap[route] ?? home;
+}
+
+class AppNavigator {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
+  static Route onGenerateRoute(RouteSettings settings) {
+    final arguments = settings.arguments as List<dynamic>?;
+    final title = arguments?.first ?? '';
+    final id = arguments?.last ?? 1;
+
+    switch (settings.name) {
+      case _Paths.boarding:
+        return FadeRoute(page: const BoardingScreen());
+
+      case _Paths.login:
+        return FadeRoute(page: const LoginScreen());
+
+      case _Paths.signUp:
+        return FadeRoute(page: const SignUpScreen());
+
+      case _Paths.home:
+        return FadeRoute(page: const HomeScreen());
+
+      case _Paths.home:
+      default:
+        return FadeRoute(page: const HomeScreen());
+    }
+  }
+
+  static Future? push<T>(Routes route, [T? arguments]) =>
+      state?.pushNamed(_Paths.of(route), arguments: arguments);
+
+  static Future? replaceWith<T>(Routes route, [T? arguments]) =>
+      state?.pushReplacementNamed(_Paths.of(route), arguments: arguments);
+
+  static void pop() => state?.pop();
+
+  static NavigatorState? get state => navigatorKey.currentState;
+}
