712a9e0442a2745d6f0f823d7dbde8fc2928af62
ParimSsme <parima.ssme@gmail.com>
2024-03-05 13:20:52 +0430

    add tabs_main_wrapper.dart

---
+++ lib/common/widgets/tabs_main_wrapper.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_shop_app/config/colors/furniture_colors.dart';
+import 'package:furniture_shop_app/features/feature_favorites/presentation/screens/favorites_screen.dart';
+import 'package:furniture_shop_app/features/feature_home/presentation/screens/home_screen.dart';
+import '../../features/feature_notifications/presentation/screens/notifications_screen.dart';
+import '../../features/feature_profile/presentation/screens/profile_screen.dart';
+
+class TabsMainWrapper extends StatefulWidget {
+  const TabsMainWrapper({super.key});
+
+  @override
+  State<TabsMainWrapper> createState() => _TabsMainWrapperState();
+}
+
+class _TabsMainWrapperState extends State<TabsMainWrapper> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    FavoritesScreen(),
+    NotificationsScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border_outlined),
+            activeIcon: Icon(Icons.bookmark),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_none_outlined),
+            activeIcon: Icon(Icons.notifications),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+        elevation: 15,
+        backgroundColor: Colors.white,
+        currentIndex: _selectedIndex,
+        selectedItemColor: FurnitureColors.black,
+        unselectedItemColor: FurnitureColors.lightGrey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
