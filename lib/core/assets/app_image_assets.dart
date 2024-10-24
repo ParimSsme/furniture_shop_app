382ece8b1d6e8bdd8136258163eac09c3dfda266
Parima <parima.ssme@gmail.com>
2024-10-25 04:18:22 +0430

    add app assets

---
+++ lib/core/assets/app_image_assets.dart
@@ -0,0 +1,12 @@
+const _basePath = 'assets/images';
+
+/// PNG and JPG images
+class AppImageAssets {
+  static const String bed = '$_basePath/bed.png';
+  static const String chair = '$_basePath/chair.png';
+  static const String furniture = '$_basePath/furniture.png';
+  static const String introBackground = '$_basePath/intro_background.jpg';
+  static const String lamp = '$_basePath/lamp.png';
+  static const String bruno = '$_basePath/bruno.png';
+  static const String sofa = '$_basePath/sofa.png';
+}
---
+++ lib/core/assets/app_svg_assets.dart
@@ -0,0 +1,13 @@
+const _basePath = 'assets/svgs';
+
+/// SVG images
+class AppSvgAssets {
+  static const String favourite = '$_basePath/favourite.svg';
+  static const String favouriteFilled = '$_basePath/favourite_filled.svg';
+  static const String home = '$_basePath/home.svg';
+  static const String homeFilled = '$_basePath/home_filled.svg';
+  static const String notification = '$_basePath/notification.svg';
+  static const String notificationFilled = '$_basePath/notification_filled.svg';
+  static const String profile = '$_basePath/profile.svg';
+  static const String profileFilled = '$_basePath/profile_filled.svg';
+}
