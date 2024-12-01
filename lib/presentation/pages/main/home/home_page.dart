dacc6ebd67d56577c533c05a8c18f72cd0cfb2bb
Parima <parima.ssme@gmail.com>
2024-12-01 12:06:32 +0430

    refactor home page codes

--- lib/presentation/pages/main/home/home_page.dart
+++ lib/presentation/pages/main/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_shop_app/core/extensions/widget_extension.dart';
 import 'package:furniture_shop_app/presentation/controllers/main/home_controller.dart';
-import 'package:furniture_shop_app/presentation/pages/main/home/widgets/products_list.dart';
+import 'package:furniture_shop_app/presentation/pages/main/home/widgets/product_list_item.dart';
 import '../../../../core/theme/app_text_theme.dart';
 import 'widgets/category_list.dart';
 
@@ -14,26 +14,24 @@ class HomePage extends StatelessWidget {
     final controller = HomeController.to;
 
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        centerTitle: true,
+        title:  Column(
+          children: [
+            Text('Make Home', style: kBodyNunitoTextStyle),
+            Text('BEAUTIFUL', style: kBlackGelasioMediumTitleStyle),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: controller.onMyCartClick,
+            icon: const Icon(Icons.shopping_cart_outlined),),
+        ],
+      ),
       body: SafeArea(
         child: Column(
           children: [
-
-            /// Header
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-                Column(
-                  children: [
-                    Text('Make Home', style: kBodyNunitoTextStyle),
-                    Text('BEAUTIFUL', style: kBlackGelasioMediumTitleStyle),
-                  ],
-                ),
-                IconButton(
-                    onPressed: controller.onMyCartClick,
-                    icon: const Icon(Icons.shopping_cart_outlined)),
-              ],
-            ),
 
             /// Category Filters
             const SizedBox(
@@ -42,8 +40,24 @@ class HomePage extends StatelessWidget {
             ),
 
             /// Furniture List
-            const Expanded(
-              child: ProductsList(),
+            Expanded(
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                childAspectRatio: 0.61,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                children: List.generate(
+                  controller.products.length,
+                      (index) {
+                    final entity = controller.products[index];
+                    return GestureDetector(
+                      onTap: () => controller.onItemClick(id: entity.id),
+                      child: ProductListItem(entity: entity),
+                    );
+                  },
+                ),
+              ),
             )
           ],
         ).defaultScreenPadding(),
--- lib/presentation/pages/main/home/widgets/product_list_item.dart
+++ lib/presentation/pages/main/home/widgets/product_list_item.dart
@@ -2,43 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:furniture_shop_app/core/widgets/app_icon_button.dart';
 import 'package:furniture_shop_app/domain/entities/product_entity.dart';
-import 'package:furniture_shop_app/presentation/controllers/main/home_controller.dart';
-import 'package:get/get.dart';
 import '../../../../../core/assets/app_icon_assets.dart';
 import '../../../../../core/theme/app_text_theme.dart';
 
-class ProductsList extends StatelessWidget {
-  const ProductsList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = HomeController.to;
-
-    return Obx(
-      () => GridView.count(
-        shrinkWrap: true,
-        crossAxisCount: 2,
-        childAspectRatio: 0.61,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        children: List.generate(
-          controller.products.length,
-          (index) {
-            final entity = controller.products[index];
-            return GestureDetector(
-              onTap: () => controller.onItemClick(id: entity.id),
-              child: _ProductListItem(entity: entity),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class _ProductListItem extends StatelessWidget {
+class ProductListItem extends StatelessWidget {
   final ProductEntity entity;
-  const _ProductListItem({
+  const ProductListItem({
+    super.key,
     required this.entity,
   });
 
@@ -71,7 +41,8 @@ class _ProductListItem extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(entity.name, style: kBodyNunitoTextStyle),
-              Text('\$${entity.price}'.padRight(2, '0'), style: kBlackNunitoSmallTitleStyle),
+              Text('\$${entity.price}'.padRight(2, '0'),
+                  style: kBlackNunitoSmallTitleStyle),
             ],
           ),
         )
