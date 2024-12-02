15e4d4fd46fea51caba58452a61aa9634dbd8e71
Parima <parima.ssme@gmail.com>
2024-12-02 12:54:54 +0430

    update home list data on filter

--- lib/presentation/pages/main/home/home_page.dart
+++ lib/presentation/pages/main/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:furniture_shop_app/core/extensions/widget_extension.dart';
 import 'package:furniture_shop_app/presentation/controllers/main/home_controller.dart';
 import 'package:furniture_shop_app/presentation/pages/main/home/widgets/product_list_item.dart';
+import 'package:get/get.dart';
 import '../../../../core/theme/app_text_theme.dart';
 import 'widgets/category_list.dart';
 
@@ -10,14 +11,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final controller = HomeController.to;
 
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
         centerTitle: true,
-        title:  Column(
+        title: Column(
           children: [
             Text('Make Home', style: kBodyNunitoTextStyle),
             Text('BEAUTIFUL', style: kBlackGelasioMediumTitleStyle),
@@ -26,13 +26,13 @@ class HomePage extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: controller.onMyCartClick,
-            icon: const Icon(Icons.shopping_cart_outlined),),
+            icon: const Icon(Icons.shopping_cart_outlined),
+          ),
         ],
       ),
       body: SafeArea(
         child: Column(
           children: [
-
             /// Category Filters
             const SizedBox(
               height: 80,
@@ -41,21 +41,23 @@ class HomePage extends StatelessWidget {
 
             /// Furniture List
             Expanded(
-              child: GridView.count(
-                shrinkWrap: true,
-                crossAxisCount: 2,
-                childAspectRatio: 0.61,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20,
-                children: List.generate(
-                  controller.products.length,
-                      (index) {
-                    final entity = controller.products[index];
-                    return GestureDetector(
-                      onTap: () => controller.onItemClick(id: entity.id),
-                      child: ProductListItem(entity: entity),
-                    );
-                  },
+              child: Obx(
+                () => GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.61,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                  children: List.generate(
+                    controller.products.length,
+                    (index) {
+                      final entity = controller.products[index];
+                      return GestureDetector(
+                        onTap: () => controller.onItemClick(id: entity.id),
+                        child: ProductListItem(entity: entity),
+                      );
+                    },
+                  ),
                 ),
               ),
             )
